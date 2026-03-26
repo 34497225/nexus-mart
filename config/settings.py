9ae84ebde8b06d1201621a203d48b0ecb21307d1
MingGuan <smg60214@gmail.com>
2026-03-27 00:11:49 +0800

    Fix: Add http scheme to CSRF_TRUSTED_ORIGINS

--- config/settings.py
+++ config/settings.py
@@ -143,6 +143,6 @@ CELERY_RESULT_BACKEND = 'redis://redis:6379/1'
 # 集中收集靜態檔案的目錄 (給 Nginx 用的)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-CSRF_TRUSTED_ORIGINS = 'http://43.212.25.222:8002'
+CSRF_TRUSTED_ORIGINS = ['http://43.212.25.222:8002']
 
 
